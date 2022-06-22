6383ef522ad19f70c7ef141cc74111adb21439d5
Marcos Vinicius <marcosvinicius@Mac-mini-de-Marcos.local>
2022-06-22 13:21:28 -0300

    Lifecycle hooks (ciclo de vida)

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
    <Pcomponente />
-
+   <LifeCycle />
 </template>
 
 <script>
     import Pcomponente from './components/p_componente.vue'
+    import LifeCycle from './components/LifeCycle.vue'
 
     export default {
       name: 'App',
       components: {
-          Pcomponente
+          Pcomponente,
+          LifeCycle
       }
     }
 
---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Meu nome é: {{name}} </h1>
+</template>
+
+<script>
+   export default {
+       name: 'LifeCycle',
+       data() {
+           return{ 
+             name: 'ainda nao sei'
+          }
+       },
+        created (){
+            setTimeout (()=> {
+                this.name = 'Marcos'
+            }, 2000)
+         },
+        mounted(){
+           setTimeout (() =>{
+               this.name = 'O men'
+           }, 3000) 
+        }
+   }
+</script>
